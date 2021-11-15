8bdfbb5f37dd7eb25bc12ad009247a78641a0be7
James Tabor <jtabor@my365.bellevue.edu>
2021-11-14 19:09:12 -0500

    Updated for Sprint 2

---
+++ GradeBookApp/Student.java
@@ -0,0 +1,66 @@
+/*
+ * Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+ * Comprehensive Version (12th ed.). Pearson Education, Inc.
+ * Modified by R. Krasso 2021
+ * Additional modifications by A. Student 2021
+ */
+/*
+ * Tabor, J. (2021). CIS 505 Intermediate Java Programming. 
+ * Bellevue University.
+ */
+
+public class Student {
+    
+    private String firstName;
+    private String lastName;
+    private String course;
+    private String grade;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String course, String grade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.course = course;
+        this.grade = grade;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [course=" + course + ", firstName=" + firstName + ", grade=" + grade + ", lastName=" + lastName
+                + "]";
+    }
+}
--- GradeBookApp/TaborGradeBookApp.java
+++ GradeBookApp/TaborGradeBookApp.java
@@ -1,3 +1,14 @@
+/*
+ * Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+ * Comprehensive Version (12th ed.). Pearson Education, Inc.
+ * Modified by R. Krasso 2021
+ * Additional modifications by A. Student 2021
+ */
+/*
+ * Tabor, J. (2021). CIS 505 Intermediate Java Programming. 
+ * Bellevue University.
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
